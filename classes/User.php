77da642d539caa58298ab61926d3ccc10b800d49
Álvaro Hernández Colino <alvarohcolino@gmail.com>
2023-11-27 19:39:01 +0100

    Finish login page styling

---
+++ classes/User.php
@@ -0,0 +1,7 @@
+<?php
+    class User {
+        public $user_name;
+        public $user_password;
+        public $user_email;
+    }
+?>
--- login.php
+++ login.php
@@ -5,11 +5,16 @@ includeTemplate('header');
 
 <main class="login">
     <form method="POST">
-        <label for="email">E-mail</label>
-        <input type="e-mail" id="email" name="email">
+        <fieldset>
+            <label for="email">E-mail</label>
+            <input type="e-mail" id="email" name="email">
 
-        <label for="password">Password</label>
-        <input type="password" id="password" name="password">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password">
+        </fieldset>
+
+        <input type="submit" value="Enter">
+        <p class="create-account-paragraph">Don't have an account, <a href="register.php">create one</a></p>
     </form>
 </main>
 
