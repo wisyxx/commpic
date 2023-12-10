f1f4209cf8ff4c7fadbcb0860704a6b021885953
Álvaro Hernández Colino <alvarohcolino@gmail.com>
2023-12-10 19:58:08 +0100

    Add log-out functionality, topics.php styles & mixins for buttons

--- topics.php
+++ topics.php
@@ -4,15 +4,18 @@ require 'includes/app.php';
 session_start();
 
 if (!isset($_SESSION['user-name'])) {
-    header('Location: /login.php?err=0');
+    header('Location: /login.php');
 }
 
 includeTemplate('header');
 ?>
 
 <header>
-    <h1>Welcome <?php echo $_SESSION['user-name']; ?>!</h1>
-    <form method="POST">
-        <input type="submit" value="Sign off">
-    </form>
+    <nav class="topics-nav">
+        <h1>Welcome <?php echo $_SESSION['user-name']; ?>!</h1>
+        <div class="actions">
+            <img class="user-ppf" src="src/images/avatars/myppf.JPG" alt="User profile picture">
+            <a href="log-out.php">Log out</a>
+        </div>
+    </nav>
 </header>
