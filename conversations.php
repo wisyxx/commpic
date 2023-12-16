6fd67b77f5bdc8c934a3b16b09c63a3a5ba5fe9a
Álvaro Hernández Colino <alvarohcolino@gmail.com>
2023-12-16 13:09:13 +0100

    Finish conversations frontend

--- conversations.php
+++ conversations.php
@@ -12,13 +12,158 @@ includeTemplate('header');
 
 
 <header>
-    <nav class="navigation">
-        <h1 class="navigation__mesaje">Welcome <?php echo $_SESSION['user-name']; ?>!</h1>
+    <nav class="conversations-navigation">
+        <h1 class="conversations-navigation__mesaje">Welcome <?php echo $_SESSION['user-name']; ?>!</h1>
         <?php includeTemplate('user-menu') ?>
     </nav>
 </header>
 
-
+<main class="conversations">
+    <div class="conversation">
+        <h1>Title examle</h1>
+        <div class="conversation__data">
+            <p>Users: 5</p>
+            <div class="tags">
+                <p class="no-margin">#tag1</p>
+                <p class="no-margin">#tag2</p>
+                <p class="no-margin">#tag3</p>
+                <p class="no-margin">#tag4</p>
+            </div>
+        </div>
+    </div>
+    <div class="conversation">
+        <h1>Title examle</h1>
+        <div class="conversation__data">
+            <p>Users: 5</p>
+            <div class="tags">
+                <p class="no-margin">#tag1</p>
+                <p class="no-margin">#tag2</p>
+                <p class="no-margin">#tag3</p>
+                <p class="no-margin">#tag4</p>
+            </div>
+        </div>
+    </div>
+    <div class="conversation">
+        <h1>Title examle</h1>
+        <div class="conversation__data">
+            <p>Users: 5</p>
+            <div class="tags">
+                <p class="no-margin">#tag1</p>
+                <p class="no-margin">#tag2</p>
+                <p class="no-margin">#tag3</p>
+                <p class="no-margin">#tag4</p>
+            </div>
+        </div>
+    </div>
+    <div class="conversation">
+        <h1>Title examle</h1>
+        <div class="conversation__data">
+            <p>Users: 5</p>
+            <div class="tags">
+                <p class="no-margin">#tag1</p>
+                <p class="no-margin">#tag2</p>
+                <p class="no-margin">#tag3</p>
+                <p class="no-margin">#tag4</p>
+            </div>
+        </div>
+    </div>
+    <div class="conversation">
+        <h1>Title examle</h1>
+        <div class="conversation__data">
+            <p>Users: 5</p>
+            <div class="tags">
+                <p class="no-margin">#tag1</p>
+                <p class="no-margin">#tag2</p>
+                <p class="no-margin">#tag3</p>
+                <p class="no-margin">#tag4</p>
+            </div>
+        </div>
+    </div>
+    <div class="conversation">
+        <h1>Title examle</h1>
+        <div class="conversation__data">
+            <p>Users: 5</p>
+            <div class="tags">
+                <p class="no-margin">#tag1</p>
+                <p class="no-margin">#tag2</p>
+                <p class="no-margin">#tag3</p>
+                <p class="no-margin">#tag4</p>
+            </div>
+        </div>
+    </div>
+    <div class="conversation">
+        <h1>Title examle</h1>
+        <div class="conversation__data">
+            <p>Users: 5</p>
+            <div class="tags">
+                <p class="no-margin">#tag1</p>
+                <p class="no-margin">#tag2</p>
+                <p class="no-margin">#tag3</p>
+                <p class="no-margin">#tag4</p>
+            </div>
+        </div>
+    </div>
+    <div class="conversation">
+        <h1>Title examle</h1>
+        <div class="conversation__data">
+            <p>Users: 5</p>
+            <div class="tags">
+                <p class="no-margin">#tag1</p>
+                <p class="no-margin">#tag2</p>
+                <p class="no-margin">#tag3</p>
+                <p class="no-margin">#tag4</p>
+            </div>
+        </div>
+    </div>
+    <div class="conversation">
+        <h1>Title examle</h1>
+        <div class="conversation__data">
+            <p>Users: 5</p>
+            <div class="tags">
+                <p class="no-margin">#tag1</p>
+                <p class="no-margin">#tag2</p>
+                <p class="no-margin">#tag3</p>
+                <p class="no-margin">#tag4</p>
+            </div>
+        </div>
+    </div>
+    <div class="conversation">
+        <h1>Title examle</h1>
+        <div class="conversation__data">
+            <p>Users: 5</p>
+            <div class="tags">
+                <p class="no-margin">#tag1</p>
+                <p class="no-margin">#tag2</p>
+                <p class="no-margin">#tag3</p>
+                <p class="no-margin">#tag4</p>
+            </div>
+        </div>
+    </div>
+    <div class="conversation">
+        <h1>Title examle</h1>
+        <div class="conversation__data">
+            <p>Users: 5</p>
+            <div class="tags">
+                <p class="no-margin">#tag1</p>
+                <p class="no-margin">#tag2</p>
+                <p class="no-margin">#tag3</p>
+                <p class="no-margin">#tag4</p>
+            </div>
+        </div>
+    </div>
+    <div class="conversation">
+        <h1>Title examle</h1>
+        <div class="conversation__data">
+            <p>Users: 5</p>
+            <div class="tags">
+                <p class="no-margin">#tag1</p>
+                <p class="no-margin">#tag2</p>
+                <p class="no-margin">#tag3</p>
+                <p class="no-margin">#tag4</p>
+            </div>
+        </div>
+    </div>
+</main>
 
 <?php
 includeTemplate('footer');
